536ac72ca0cb1131ec0e33257e464a04286bd378
Magenta-coder <nicosebastian509@gmail.com>
2022-09-15 08:02:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10794/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10794
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan sehari-hari untuk membuat hari makin indah"),
+        Task("Chapter 2", "Nguli Genshin", "Mencari primogen"),
+        Task("Chapter 3", "Cape Habis Nguli", "Selesai nguli minum kopi"),
+        Task("Chapter 4", "Dimana mama Ei?", "Buat adu mekanik"),
+        Task("Chapter 5", "Upload ke Tiktok", "Buat pamer ke viewer")
+    )
+}
